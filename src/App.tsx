75652e3cf773f121451a2fd9e877bb4763e521bc
spcsabi <csabaspengler@gmail.com>
2024-06-04 08:49:23 +0200

    build app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import Map from "./Components/Map"
+import Map from "./Components/Map";
 import Fight from "./Components/Fight";
+import { useState } from "react";
+
 
 export default function App() {
-  return (
-    <div>
-      <Map/>
-      <Fight n={1} />
-    </div>
-      
-  );
+  
+  const [locationSelection, setLocationSelection] = useState(true);
+  const [locationNumber, setLocationNumber] = useState(null);
+
+  if (locationSelection) {
+    return (
+      <div>
+        <Map />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Fight n={locationNumber} />
+      </div>
+    );
+  }
 }
