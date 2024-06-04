3724ed77d82c1a415ba0f62700ed56f2742194ae
spcsabi <csabaspengler@gmail.com>
2024-06-04 08:35:56 +0200

    add background img to fight component

--- src/Components/Fight.tsx
+++ src/Components/Fight.tsx
@@ -1,3 +1,14 @@
-export default function Fight(): React.ReactNode {
-  return <div></div>;
+import { useEffect, useState } from "react";
+import { gifUrl } from "../data/backgrounds";
+
+export default function Fight({ n }: { n: number }): React.ReactNode {
+  const [background, setBackground] = useState("");
+  useEffect(() => {
+    setBackground(gifUrl[n]);
+  }, [n]);
+  return (
+    <div>
+      <img src={background} alt="background" />
+    </div>
+  );
 }
