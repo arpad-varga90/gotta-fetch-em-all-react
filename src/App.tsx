61c43d7d663b3f323ceae5e3d9536ace3eae4057
spcsabi <csabaspengler@gmail.com>
2024-06-04 08:35:38 +0200

    prittier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Map from "./Components/Map"
-
+import Fight from "./Components/Fight";
 
 export default function App() {
   return (
     <div>
       <Map/>
+      <Fight n={1} />
     </div>
       
   );
--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,15 +1,20 @@
 import Location from "./Location";
 import imgUrl from "../assets/PokeMap.png";
-import { coordinates, mapPins} from "../data/coordinates";
-
+import { coordinates, mapPins } from "../data/coordinates";
 
 export default function Map() {
   return (
-    <div className="relative "
-    >
-      <img src={imgUrl} alt="PokeMap" className="size-full"/>
+    <div className="relative ">
+      <img src={imgUrl} alt="PokeMap" className="size-full" />
       {[...Array(20).keys()].map((i) => {
-        return <Location key={i + 1} n={i + 1} coordinates={coordinates} mapPins={mapPins} />;
+        return (
+          <Location
+            key={i + 1}
+            n={i + 1}
+            coordinates={coordinates}
+            mapPins={mapPins}
+          />
+        );
       })}
     </div>
   );
