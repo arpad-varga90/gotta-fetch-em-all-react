aa9b6efbdffd7ffd0fb26137d724b4cabb87210e
arpad.varga90 <arpad.varga90@gmail.com>
2024-06-05 08:46:43 +0200

    add more fetch

--- src/Components/PokemonMain.tsx
+++ src/Components/PokemonMain.tsx
@@ -1,4 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
+import { PokemonDetails } from "../Types/types";
+
+async function fetchPokeDetails(): Promise<PokemonDetails> {
+  const id = getRandNumber(1, 1025);
+  const URL = "https://pokeapi.co/api/v2/pokemon/";
+  const response = await fetch(URL + id);
+  const data = response.json();
+  return data;
+}
+
 function usePokemon() {
   return useQuery({
     queryKey: ["todos"],
